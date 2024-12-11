b58bd55973997e4454e657838817a53e61df8984
Clément.A <125081819+ITClmt@users.noreply.github.com>
2024-12-11 16:42:28 +0100

    initial commit

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,4 +1,8 @@
-function PokemonCard({ pokemon }) {
+interface PokemonCardProps {
+  pokemon: { name: string; imgSrc?: string };
+}
+
+function PokemonCard({ pokemon }: PokemonCardProps) {
   return (
     <figure>
       {pokemon.imgSrc ? (
